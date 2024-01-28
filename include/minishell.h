5d66cb815ff56be9ff39fd77c7ab041dd6bdf96c
kyusulee <rbt2775@cau.ac.kr>
2024-01-28 21:28:59 +0900

    update: minishell.h because of list nodes structure

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: kyusulee <kyusulee@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/26 19:57:14 by kyusulee          #+#    #+#             */
-/*   Updated: 2024/01/26 19:59:08 by kyusulee         ###   ########.fr       */
+/*   Updated: 2024/01/28 21:28:05 by kyusulee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,24 @@
 
 # include "../kyusulib/kyusulib.h"
 # include <stdio.h>
+
+typedef struct s_pipe
+{
+	t_list	*words;
+	t_list	*rds;
+}			t_pipe;
+
+typedef enum e_rd_category
+{
+	IN_RD = 0,
+	OUT_RD,
+	APPEND_RD
+}			t_rd_category;
+
+typedef struct s_rd
+{
+	t_rd_category	category;
+	char			*word;
+}				t_rd;
 
 #endif
