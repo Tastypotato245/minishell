f3065ab589873f29df1374a87ad30021d7da230c
Youngho Cho <dominoyh5@gmail.com>
2024-01-28 18:30:41 +0900

    update: create_word_token() handling quote (backslash is not handled)

--- src/tokenize_create_token.c
+++ src/tokenize_create_token.c
@@ -109,18 +109,51 @@ t_token	*create_here_doc_token(size_t *i)
 	return (token);
 }
 
+static int	handling_quote(const char *line, size_t *i, t_vector *vector)
+{
+	const char	quote = line[*i];
+
+	push_back(vector, line[*i]);
+	(*i)++;
+	while (line[*i] != '\0' && line[*i] != quote)
+	{
+		push_back(vector, line[*i]);
+		(*i)++;
+	}
+	if (line[*i] == '\0')
+		return (-1);
+	push_back(vector, line[*i]);
+	(*i)++;
+	return (0);
+}
+
 t_token	*create_word_token(const char *line, size_t *i)
 {
 	t_token		*token;
 	t_vector	vector;
+	int			ret;
 
 	token = malloc(sizeof(t_token));
 	token->category = T_WORD;
+	token->content = NULL;
 	init_vector(&vector);
 	while (line[*i] != '\0' && ft_is_metacharacter(line[*i]) == 0)
 	{
-		push_back(&vector, line[*i]);
-		(*i)++;
+		if (line[*i] == '\"' || line[*i] == '\'')
+		{
+			ret = handling_quote(line, i, &vector);
+			if (ret == -1)
+			{
+				destroy_vector(&vector);
+				destroy_token(token);
+				return (create_error_token());
+			}
+		}
+		else
+		{
+			push_back(&vector, line[*i]);
+			(*i)++;
+		}
 	}
 	push_back(&vector, '\0');
 	token->content = vector.data;
