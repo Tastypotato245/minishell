0335b312d6cce8796388c61f8c95f69888361778
Youngho Cho <dominoyh5@gmail.com>
2024-02-05 17:58:08 +0900

    update: filename_expansion() works

--- src/expansion_filename.c
+++ src/expansion_filename.c
@@ -15,27 +15,61 @@
 #include <dirent.h>
 #include <kyusulib.h>
 
-//static int	is_matched(char *word, char *name)
-//{
-//}
+static int	is_matched(const char *pattern, const char *string)
+{
+	while (*pattern && *string)
+	{
+		if (*pattern == '*')
+		{
+			if (is_matched(pattern + 1, string))
+				return (1);
+			++string;
+		}
+		else
+		{
+			if (*pattern != *string)
+				return (0);
+			++pattern;
+			++string;
+		}
+	}
+	if (*pattern == '*' && *(pattern + 1) == '\0')
+		return (1);
+	return (*pattern == *string);
+}
 
 t_exe_lst	*filename_expansion(char *word)
 {
 	const size_t	wordlen = ft_strlen(word);
-	t_exe_lst	*exes;
+	const int		only_dir = word[wordlen - 1] == '/';
+	t_exe_lst		*exes;
+	DIR				*dir;
+	struct dirent	*entry;
 
-	(void)word;
+	if (only_dir)
+		word[wordlen - 1] = '\0';
 	exes = new_exe_lst();
-    DIR *dir = null_guard(opendir("."), PROGRAM_NAME, "filename_expansion().");
-    struct dirent *entry;
-    while ((entry = readdir(dir)) != NULL) {
-		if (word[0] != '.' && entry->d_name[0] == '.')
+	dir = null_guard(opendir("."), PROGRAM_NAME, "filename_expansion().");
+	entry = readdir(dir);
+	while (entry != NULL)
+	{
+		if ((word[0] != '.' && entry->d_name[0] == '.')
+			|| (only_dir && entry->d_type != DT_DIR))
+		{
+			entry = readdir(dir);
 			continue ;
-		if (word[wordlen - 1] == '/' && entry->d_type != DT_DIR)
-			continue ;
-		// if (is_matched(word, entry->d_name))
-			exe_lst_new_back(exes, ft_strdup(entry->d_name));
-    }
-    closedir(dir);
+		}
+		if (is_matched(word, entry->d_name))
+		{
+			if (only_dir)
+				exe_lst_new_back(exes, ft_strjoin(entry->d_name, "/"));
+			else
+				exe_lst_new_back(exes, ft_strdup(entry->d_name));
+		}
+		entry = readdir(dir);
+	}
+	closedir(dir);
+	if (exes->size == 0)
+		exe_lst_new_back(exes, ft_strdup(word));
 	return (exes);
 }
