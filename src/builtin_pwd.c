a7eb5c64161a7e86ebf56b43c71c8a7c47e0d594
kyusulee <rbt2775@cau.ac.kr>
2024-02-05 13:35:29 +0900

    fix: echo and exit

--- src/builtin_pwd.c
+++ src/builtin_pwd.c
@@ -6,7 +6,7 @@
 /*   By: kyusulee <kyusulee@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 18:50:42 by kyusulee          #+#    #+#             */
-/*   Updated: 2024/02/01 20:26:21 by kyusulee         ###   ########.fr       */
+/*   Updated: 2024/02/05 13:32:02 by kyusulee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,13 @@ int	builtin_pwd(void)
 {
 	char	*buf;
 
-	buf = null_guard(getcwd(NULL, 0), PROGRAM_NAME, "builtin_pwd().");
-	printf("%s\n", buf);
-	free(buf);
+	buf = getcwd(NULL, 0);
+	if (buf == NULL)
+		print_error(BTIN_PWD, NULL, strerror(errno));
+	else
+	{
+		printf("%s\n", buf);
+		free(buf);
+	}
 	return (0);
 }
