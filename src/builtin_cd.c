e98bfc6bfd827a4b1dc088fcd9ec222514a3eafb
kyusulee <rbt2775@cau.ac.kr>
2024-02-05 14:48:27 +0900

    fix: diet builtin cd -> simplification

--- src/builtin_cd.c
+++ src/builtin_cd.c
@@ -6,86 +6,56 @@
 /*   By: kyusulee <kyusulee@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 18:50:26 by kyusulee          #+#    #+#             */
-/*   Updated: 2024/02/05 13:32:04 by kyusulee         ###   ########.fr       */
+/*   Updated: 2024/02/05 14:47:59 by kyusulee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <builtin.h>
 #include <dirent.h>
 
-static void	update_pwd(t_pair *n_pwd, t_pair *o_pwd, char *new_pwd)
+static void	set_pwd(char mod, t_pair *n_pwd, t_pair *o_pwd)
 {
 	char	*buf;
 
-	if (o_pwd != NULL)
+	if (mod == 'o')
 	{
-		free(o_pwd->val);
-		o_pwd->val = NULL;
+		if (n_pwd != NULL && o_pwd != NULL && n_pwd->val !=NULL)
+		{
+			free(o_pwd->val);
+			o_pwd->val = ft_strdup(n_pwd->val);
+		}
+	}
+	else if (mod == 'n' && n_pwd != NULL)
+	{
 		buf = getcwd(NULL, 0);
 		if (buf == NULL)
-			print_error(BTIN_CD, "error retrieving current directory: getcwd", \
-					strerror(errno));
-		o_pwd->val = buf;
+			print_error(BTIN_CD, NULL, \
+					"error retrieving current directory: getcwd");
+		else
+		{
+			free(n_pwd->val);
+			n_pwd->val = buf;
+		}
 	}
-	if (n_pwd != NULL)
-	{
-		free(n_pwd->val);
-		n_pwd->val = new_pwd;
-	}
+}
+
+static int	absolute_chdir(t_pair *n_pwd, t_pair *o_pwd, char *target_dir)
+{
+	set_pwd('o', n_pwd, o_pwd);
+	if (chdir(target_dir) == -1)
+		return (return_handler(1, BTIN_CD, target_dir, strerror(errno)));
+	set_pwd('n', n_pwd, o_pwd);
+	return (0);
 }
 
 static int	go_home(t_dict *env, t_pair *n_pwd, t_pair *o_pwd)
 {
 	char	*target_dir;
-	DIR		*dir;
-	char	*buf;
 
-	target_dir = ft_strdup(find_val_in_dict(env, "HOME"));
+	target_dir = find_val_in_dict(env, "HOME");
 	if (target_dir == NULL)
 		return (return_handler(1, BTIN_CD, NULL, "HOME not set"));
-	dir = opendir(target_dir);
-	if (dir == NULL)
-		return (return_handler(1, BTIN_CD, target_dir, strerror(errno)));
-	else
-		closedir(dir);
-	update_pwd(n_pwd, o_pwd, target_dir);
-	if (chdir(target_dir) == -1)
-		return (return_handler(1, BTIN_CD, target_dir, strerror(errno)));
-	if (n_pwd != NULL)
-	{
-		free(n_pwd->val);
-		buf = getcwd(NULL, 0);
-		if (buf == NULL)
-			print_error(BTIN_CD, "error retrieving current directory: getcwd", \
-					strerror(errno));
-		n_pwd->val = buf;
-	}
-	return (0);
-}
-
-static int	absolute_chdir(t_pair *n_pwd, t_pair *o_pwd, char *target_dir)
-{
-	DIR		*dir;
-	char	*buf;
-
-	dir = opendir(target_dir);
-	if (dir == NULL)
-		return (return_handler(1, BTIN_CD, target_dir, strerror(errno)));
-	else
-		closedir(dir);
-	update_pwd(n_pwd, o_pwd, target_dir);
-	if (chdir(target_dir) == -1)
-		return (return_handler(1, BTIN_CD, target_dir, strerror(errno)));
-	if (n_pwd != NULL)
-	{
-		free(n_pwd->val);
-		buf = getcwd(NULL, 0);
-		if (buf == NULL)
-			print_error(BTIN_CD, "error retrieving current directory: getcwd", \
-					strerror(errno));
-		n_pwd->val = buf;
-	}
-	return (0);
+	return (absolute_chdir(n_pwd, o_pwd, target_dir));
 }
 
 static int	relative_chdir(t_exe_node *exe, t_pair *n_pwd, t_pair *o_pwd)
@@ -93,16 +63,19 @@ static int	relative_chdir(t_exe_node *exe, t_pair *n_pwd, t_pair *o_pwd)
 	char	*target_dir_part;
 	char	*target_dir;
 	char	*buf;
+	int		rt;
 
 	buf = getcwd(NULL, 0);
 	if (buf == NULL)
-		print_error(BTIN_CD, "error retrieving current directory: getcwd", \
-				strerror(errno));
+		return (return_handler(0, BTIN_CD, NULL, \
+				"error retrieving current directory: getcwd"));
 	target_dir_part = ft_strjoin(buf, "/");
 	target_dir = ft_strjoin(target_dir_part, exe->word);
 	free(buf);
 	free(target_dir_part);
-	return (absolute_chdir(n_pwd, o_pwd, target_dir));
+	rt = absolute_chdir(n_pwd, o_pwd, target_dir);
+	free(target_dir);
+	return (rt);
 }
 
 int	builtin_cd(t_dict *env, t_exe_lst *exes)
@@ -117,6 +90,6 @@ int	builtin_cd(t_dict *env, t_exe_lst *exes)
 		return (go_home(env, n_pwd, o_pwd));
 	exe = exes->head->next;
 	if (exe->word[0] == '/')
-		return (absolute_chdir(n_pwd, o_pwd, exe->word));
+		return (absolute_chdir(n_pwd, o_pwd, ft_strdup(exe->word)));
 	return (relative_chdir(exe, n_pwd, o_pwd));
 }
--- src/builtin_pwd.c
+++ src/builtin_pwd.c
@@ -6,7 +6,7 @@
 /*   By: kyusulee <kyusulee@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 18:50:42 by kyusulee          #+#    #+#             */
-/*   Updated: 2024/02/05 13:32:02 by kyusulee         ###   ########.fr       */
+/*   Updated: 2024/02/05 14:02:33 by kyusulee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,8 @@ int	builtin_pwd(void)
 
 	buf = getcwd(NULL, 0);
 	if (buf == NULL)
-		print_error(BTIN_PWD, NULL, strerror(errno));
-	else
-	{
-		printf("%s\n", buf);
-		free(buf);
-	}
+		return (return_handler(1, BTIN_PWD, NULL, strerror(errno)));
+	printf("%s\n", buf);
+	free(buf);
 	return (0);
 }
