8c187e2de93a05d839691f490446e3f8f5b689d3
Youngho Cho <dominoyh5@gmail.com>
2024-02-05 15:38:44 +0900

    update: filename_expansion() (handling hidden files and directory)

--- src/expansion_filename.c
+++ src/expansion_filename.c
@@ -10,15 +10,32 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <minishell.h>
 #include <expansion.h>
+#include <dirent.h>
+#include <kyusulib.h>
+
+//static int	is_matched(char *word, char *name)
+//{
+//}
 
 t_exe_lst	*filename_expansion(char *word)
 {
+	const size_t	wordlen = ft_strlen(word);
 	t_exe_lst	*exes;
 
 	(void)word;
 	exes = new_exe_lst();
-	exe_lst_new_back(exes, ft_strdup("hello"));
-	exe_lst_new_back(exes, ft_strdup("word"));
+    DIR *dir = null_guard(opendir("."), PROGRAM_NAME, "filename_expansion().");
+    struct dirent *entry;
+    while ((entry = readdir(dir)) != NULL) {
+		if (word[0] != '.' && entry->d_name[0] == '.')
+			continue ;
+		if (word[wordlen - 1] == '/' && entry->d_type != DT_DIR)
+			continue ;
+		// if (is_matched(word, entry->d_name))
+			exe_lst_new_back(exes, ft_strdup(entry->d_name));
+    }
+    closedir(dir);
 	return (exes);
 }
