0336cd27798d460e94f342a990b91865a95b63b1
kyusulee <rbt2775@cau.ac.kr>
2024-02-06 19:37:35 +0900

    fix: repeat redirection position modified

--- src/execute_process.c
+++ src/execute_process.c
@@ -6,7 +6,7 @@
 /*   By: kyusulee <kyusulee@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 17:42:45 by kyusulee          #+#    #+#             */
-/*   Updated: 2024/02/06 19:10:03 by kyusulee         ###   ########.fr       */
+/*   Updated: 2024/02/06 19:37:07 by kyusulee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,10 @@ void	first_child(int *fd, t_cmd_node *cmd, t_dict *env)
 	int		builtin_case;
 
 	func_guard(close(fd[0]), PROGRAM_NAME, "close(fd[0]): first_child().");
-	repeat_redirection(cmd->rds, env);
 	func_guard(dup2(fd[1], STDOUT_FILENO), PROGRAM_NAME, \
 			"dup2(fd[1]): first_child().");
 	func_guard(close(fd[1]), PROGRAM_NAME, "close(fd[1]): first_child().");
+	repeat_redirection(cmd->rds, env);
 	builtin_case = builtin_checker(cmd);
 	if (builtin_case != NONE_BTIN_CASE)
 		exit (builtin_switcher(cmd, env, builtin_case));
@@ -46,12 +46,12 @@ void	middle_child(t_info *info, int *fd, t_cmd_node *cmd, t_dict *env)
 			"dup2(info->ex_fd, STDIN_FILENO): middle_child().");
 	func_guard(close(info->ex_fd), PROGRAM_NAME, \
 			"close(info->ex_fd): middle_child().");
-	repeat_redirection(cmd->rds, env);
 	func_guard(dup2(fd[1], STDOUT_FILENO), PROGRAM_NAME, \
 			"dup2(fd[1], STDOUT_FILENO): middle_child().");
 	func_guard(close(fd[1]), PROGRAM_NAME, \
 			"close(fd[1]): middle_child().");
 	builtin_case = builtin_checker(cmd);
+	repeat_redirection(cmd->rds, env);
 	if (builtin_case != NONE_BTIN_CASE)
 		exit (builtin_switcher(cmd, env, builtin_case));
 	else
