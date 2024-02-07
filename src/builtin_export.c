bfaf9378be79f68deee55f2328f4a6cb02e0399d
kyusulee <rbt2775@cau.ac.kr>
2024-02-07 14:40:52 +0900

    fix: leaks in export final

--- src/builtin_export.c
+++ src/builtin_export.c
@@ -6,7 +6,7 @@
 /*   By: kyusulee <kyusulee@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 18:09:39 by kyusulee          #+#    #+#             */
-/*   Updated: 2024/02/07 14:32:22 by kyusulee         ###   ########.fr       */
+/*   Updated: 2024/02/07 14:39:23 by kyusulee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,7 +84,8 @@ static void	builtin_export_normal(t_dict *env, t_exe_node **tmp, size_t *i)
 	key = ft_substr((*tmp)->word, 0, *i);
 	++(*i);
 	val = ft_substr((*tmp)->word, *i, ft_strlen((*tmp)->word) - *i);
-	dict_modi_val_or_new_in_sort(env, key, val);
+	if (dict_modi_val_or_new_in_sort(env, key, val))
+		free(key);
 	(*tmp) = (*tmp)->next;
 }
 
